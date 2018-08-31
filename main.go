cec01c2be5b83f3ab43dca26aab9bcb4d3c1b600
kaio <kaio@zhaw.ch>
2018-08-31 10:42:37 +0200

    Added support for all credential types

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"code.cloudfoundry.org/credhub-cli/credhub"
 	"code.cloudfoundry.org/credhub-cli/credhub/auth"
+	"code.cloudfoundry.org/credhub-cli/credhub/credentials"
 	"fmt"
 	"github.com/atotto/clipboard"
 	"github.com/fatih/color"
@@ -48,53 +49,90 @@ func printHelp() {
 	fmt.Println("\nNote that '-V' only works when one match is found, however '-v' will still show the values even if many matches are found.")
 }
 
-func grep(searchTerm string) {
-	r, _ := ch.FindByPartialName(searchTerm)
+func grep(searchResults credentials.FindResults) {
 	if verbose {
-		for i := 0; i < len(r.Credentials); i++ {
-			c, _ := ch.GetLatestValue(r.Credentials[i].Name)
-			fmt.Println(r.Credentials[i].Name+":", color.YellowString(string(c.Value)))
+		for i := 0; i < len(searchResults.Credentials); i++ {
+			c, _ := ch.GetLatestVersion(searchResults.Credentials[i].Name)
+			fmt.Println(color.RedString(c.Name)+":", color.YellowString(getVarString(c.Name, c.Type)))
 		}
 	} else {
-		for i := 0; i < len(r.Credentials); i++ {
-			fmt.Println(r.Credentials[i].Name)
+		for i := 0; i < len(searchResults.Credentials); i++ {
+			fmt.Println(searchResults.Credentials[i].Name)
 		}
 	}
 }
 
 func getVar() {
-	c, _ := ch.GetLatestValue(os.Args[1])
-
-	//If can't find directly, search all paths
+	c, _ := ch.GetLatestVersion(os.Args[1])
+	//If can't find directly search all paths
 	if c.Name == "" {
 		r, _ := ch.FindByPartialName(os.Args[1])
 		switch len(r.Credentials) {
 		case 0:
-			fmt.Println(color.HiRedString(os.Args[1]), "not found under any path")
+			fmt.Println(color.RedString(os.Args[1]), "not found under any path")
 			return
-		case 1:
-			c, _ = ch.GetLatestValue(r.Credentials[0].Name)
 		default:
-			grep(os.Args[1])
+			grep(r)
 			return
 		}
 	}
 
 	//If we found only one matching value then copy to clipboard and print according to required verbosity
-	_ = clipboard.WriteAll(string(c.Value))
+	varString := getVarString(c.Name, c.Type)
+	copied := false
+	if c.Type == "value" || c.Type == "password" {
+		_ = clipboard.WriteAll(varString)
+		copied = true
+	}
+
 	if xVerbose {
 		fmt.Println(color.GreenString("ID:"), c.Id)
 		fmt.Println(color.GreenString("Base:"), c.Base)
 		fmt.Println(color.GreenString("Metadata:"), c.Metadata)
 		fmt.Println(color.GreenString("Name:"), color.RedString(c.Name))
 		fmt.Println(color.GreenString("Type:"), c.Type)
-		fmt.Println(color.GreenString("Value:"), color.YellowString(string(c.Value)))
+		fmt.Println(color.GreenString("Value:"), color.YellowString(varString))
 		fmt.Println(color.GreenString("Creation Date:"), c.VersionCreatedAt)
-
-		fmt.Println("Value copied clipboard!")
-	} else if verbose {
-		fmt.Println(color.RedString(c.Name)+":", color.YellowString(string(c.Value)), "(Copied!)")
+	} else if verbose || !copied {
+		fmt.Println(color.RedString(c.Name)+":", color.YellowString(varString))
 	} else {
 		fmt.Println(color.RedString(c.Name), "copied to clipboard!")
+		return
+	}
+
+	if copied {
+		fmt.Println("Value copied clipboard!")
+	}
+}
+
+func getVarString(name string, varType string) string {
+	switch varType {
+	case "value":
+		v, _ := ch.GetLatestValue(name)
+		return string(v.Value)
+	case "password":
+		v, _ := ch.GetLatestPassword(name)
+		return string(v.Value)
+	case "certificate":
+		v, _ := ch.GetLatestCertificate(name)
+		cert := v.Value
+		return fmt.Sprintf("\nca: |\n%s\ncertificate: |\n%s\nprivate_key: |\n%s", cert.Ca, cert.Certificate, cert.PrivateKey)
+	case "json":
+		v, _ := ch.GetLatestJSON(name)
+		return fmt.Sprintf("\n%v", v.Value)
+	case "rsa":
+		v, _ := ch.GetLatestRSA(name)
+		rsa := v.Value
+		return fmt.Sprintf("\npublic_key: |\n%s\nprivate_key: |\n%s", rsa.PublicKey, rsa.PrivateKey)
+	case "ssh":
+		v, _ := ch.GetLatestSSH(name)
+		ssh := v.Value
+		return fmt.Sprintf("\npublic_key: |\n%s\nprivate_key: |\n%s", ssh.PublicKey, ssh.PrivateKey)
+	case "user":
+		v, _ := ch.GetLatestUser(name)
+		user := v.Value
+		return fmt.Sprintf("\nusername: %s\npassword: %s", user.Username, user.Password)
+	default:
+		return ""
 	}
 }
